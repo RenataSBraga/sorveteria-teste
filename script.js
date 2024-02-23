62944215ce8b1d03ae6d482ac0dcb7185c343894
RenataSBraga <147353628+RenataSBraga@users.noreply.github.com>
2024-02-23 12:50:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector('.menu');
+const nav-menu = document.querySelector('.nav-menu');
+
+menu.addEventListener('click',() >={
+    menu.classList.toggle('ativo');
+    nav-menu.classList.toggle('ativo'));
+}
